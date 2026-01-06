270bdd551c497d0bd8960f2c71374c1db2046414
Mohamed amaan <mohamedamaan319@gmail.com>
2026-01-06 21:33:15 +0530

    web analytics updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -76,6 +77,7 @@ export default function RootLayout({
           ]}
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
